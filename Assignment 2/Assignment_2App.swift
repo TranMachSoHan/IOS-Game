7567ca099f28d9094efe2d8040df36de365a9e66
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2023-08-07 22:53:18 +0700

    Initial Commit

---
+++ Assignment 2/Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_2App.swift
+//  Assignment 2
+//
+//  Created by macOS on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
