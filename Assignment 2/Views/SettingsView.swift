027ed8419348b7e76280ca27faab33c90586e3ee
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2023-08-29 15:03:06 +0700

    Add Dark Mode Toggle

--- Assignment 2/Views/SettingsView.swift
+++ Assignment 2/Views/SettingsView.swift
@@ -6,7 +6,13 @@ struct SettingsView: View {
     var body: some View {
         VStack(alignment: .leading) {
             Text("Settings")
-          
+            DarkModeToggleView(darkMode: $gameSettings.darkMode)
+                .onTapGesture {
+                    withAnimation(Animation.easeIn){
+                        gameSettings.darkmode.toggle()
+                    }
+                }
+                
         }
     }
 }
