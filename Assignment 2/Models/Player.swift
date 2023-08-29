54208122a723bbda6a3cabcb3813cb25ed45069d
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2023-08-29 14:41:44 +0700

    Update Player.swift

--- Assignment 2/Models/Player.swift
+++ Assignment 2/Models/Player.swift
@@ -9,30 +9,35 @@
   Last modified: 02/09/2023
   Acknowledgement:
     https://www.hackingwithswift.com/books/ios-swiftui/how-to-combine-core-data-and-swiftui
+    https://github.com/PScottZero/Checkmate/blob/master/Checkmate/Settings/GameSettings.swift
 */
 
 import SwiftUI
 
 class CurrentPlayer: ObservableObject  {
-    @Published var id: String = ""
+    @Published var id: String {
+      didSet {
+            UserDefaults.standard.set(id, forKey: "current_player")
+        }
+    }
     @Published var name: String = ""
     @Published var imageName: String = ""
     @Published var gameLevel: Int = 0
     @Published var badges: [String] = []
-    
-    var image : Image {
-        return Image(imageName)
+
+    init() {
+        id = UserDefaults.standard.string(forKey: "current_player") ?? ""
     }
-    func CurrentPlayer(name: String, chip: Int) {
-        self.name = name
-        self.gameLevel = 0
-    }
-    
+  
     func setPlayer(player: Player){
         self.id = player.id?.uuidString ?? ""
         self.name = player.name ?? ""
         self.imageName = player.imageName ?? ""
         let badge = player.badges ?? []
         self.badges = badge.compactMap { $0 as String }
+    }
+
+    var image : Image {
+        return Image(imageName)
     }
 }
