03e2b3d3090862b1ccd47b8b4f5fad08d53b0449
TranMachSoHan <50974854+TranMachSoHan@users.noreply.github.com>
2023-08-29 14:50:32 +0700

    Create SettingsView.swift

---
+++ Assignment 2/Views/SettingsView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct SettingsView: View {
+    @ObservedObject var gameSettings: GameSettings
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Settings")
+          
+        }
+    }
+}
